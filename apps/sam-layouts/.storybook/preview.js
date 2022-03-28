4daf732ecfba4502cb099c75246b23d2c67a1e4d
Carlos Valle <carlos.m.valle@gmail.com>
2022-03-28 01:07:48 -0400

    feat: add syntax highlight in storybook

--- apps/sam-layouts/.storybook/preview.js
+++ apps/sam-layouts/.storybook/preview.js
@@ -1,4 +1,9 @@
 import { setCompodocJson } from '@storybook/addon-docs/angular';
 import docJson from '../../../documentation.json';
 
+import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter';
+import typescript from 'react-syntax-highlighter/dist/esm/languages/prism/typescript';
+
 setCompodocJson(docJson);
+
+SyntaxHighlighter.registerLanguage('typescript', typescript);
